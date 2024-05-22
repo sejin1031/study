35ac7a6c40fd501758ee650a816cc0197f5b77b8
sejin1031 <sejin1031@utplus.co.kr>
2024-05-22 16:48:49 +0900

    tiktacto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,111 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  renderSquare(i) {
+    return <Square 
+    value={this.state.squares[i]} 
+    onClick={() => this.handleClick(i)}
+    />
+  }
+
+  render () {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) { 
+      status = `Winner: ${winner}`
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
+    
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render () {
+    return (
+      <div className="game">
+        <div className="game">
+          <Board />
+        </div>
+        <div className='gmae-info'>
+          <div></div>
+          <ol></ol>
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
@@ -7,7 +113,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to rdddeload.
         </p>
         <a
           className="App-link"
@@ -22,4 +128,4 @@ function App() {
   );
 }
 
-export default App;
+export default Game;
